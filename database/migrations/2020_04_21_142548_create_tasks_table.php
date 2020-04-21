03a22ce0bf41976b579d78fdd0aee295d52fab7e
artur <arthurgevorgyan1992@gmail.com>
2020-04-21 19:02:12 +0400

    Make task table,controller and model

---
+++ database/migrations/2020_04_21_142548_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->index()->unsigned();
+            $table->integer('user_assign_id')->index();
+            $table->string('title');
+            $table->tinyInteger('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
